7a11a358756d5b736a7c35bebab3e60d65a94973
Anh Cao <anhcao563@gmail.com>
2022-02-18 20:28:56 +0200

    Add Tooltip and SaveIcon component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { useState } from 'react';
 import './App.css';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import DeleteIcon from '@material-ui/icons/Delete';
 import TextField from '@material-ui/core/TextField';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import { Tooltip } from '@material-ui/core';
+import IconButton from '@material-ui/core/IconButton';
+import DeleteIcon from '@material-ui/icons/Delete';
+import SaveIcon from '@material-ui/icons/Save';
 
 function App() {
   const [todo, setTodo] = useState({description: '', date: ''});
@@ -18,6 +19,7 @@ function App() {
 
   const addTodo = () => {
     setTodos([...todos, todo]);
+    setTodo({description: '', date: ''});
   }
 
   const deleteTodo = (row) => {
@@ -33,26 +35,43 @@ function App() {
           </Typography>
         </Toolbar>
       </AppBar>
-      <TextField style={{marginRight: 10}} label="Description" name="description" value={todo.description} onChange={inputChanged}/>
-      <TextField style={{marginRight: 10}} label="Date" name="date" value={todo.date} onChange={inputChanged}/>
-      <Button style={{margin: 10}} color="primary" variant='outlined' onClick={addTodo}>Add</Button>
-      <table>
-        <tbody>
-       {
-          todos.map((todo, index) => 
-            <tr key={index}>
-              <td>{todo.description}</td>
-             <td>{todo.date}</td>
-             <td>
-                <IconButton size="small" color="secondary" onClick={() => deleteTodo(index)}>
-                  <DeleteIcon />
-                </IconButton>
-             </td>
-            </tr>)
-        }
-        </tbody>
-      </table>
-    </div>
+      <TextField 
+        style={{marginRight: 10}} 
+        label="Description" 
+        name="description" 
+        value={todo.description} 
+        onChange={inputChanged} 
+      />
+     <TextField 
+       style={{marginRight: 10}} 
+       label="Date" 
+       name="date" 
+       value={todo.date} 
+       onChange={inputChanged}
+      />
+     <IconButton style= {{margin: 10}} color="primary" variant="outlined" onClick={addTodo}>
+       <SaveIcon/> Add
+     </IconButton>
+     <table>
+    <tbody>
+    {
+      todos.map((todo, index) => 
+      <tr key={index}>
+        <td>{todo.description}</td>
+        <td>{todo.date}</td>
+        <td>
+          <Tooltip title="Delete todo">
+            <IconButton size="small" color="secondary" onClick={() => deleteTodo(index)}>
+              <DeleteIcon/>
+            </IconButton>
+          </Tooltip>
+          
+        </td>
+      </tr>)
+     }
+     </tbody>
+   </table>
+  </div>
   );
 }
 
